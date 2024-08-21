f4f734061a7d504f1367f57f6037fb6106c2daaa
unknown <ilianjesus14@outlook.com>
2024-08-21 17:45:44 -0600

    Registro de usuarios

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -4,5 +4,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloWorld),
+    path('', views.home),
+    path('signup/', views.signup),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 # Create your views here.
-def helloWorld(request):
-    return HttpResponse('Hello World')
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+        'form': UserCreationForm
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                ##Cramos el usuario
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                ##Guardamos el usuarioF
+                user.save() 
+                return HttpResponse('Usuario creado satisfactoriamente')
+            except:
+                return HttpResponse('El usuario ya existe')
+        return HttpResponse('Las contraseñas no coinciden')
+
+        
+    
