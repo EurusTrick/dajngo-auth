ac7521d74acfe29d6b782724986de48149b0b569
unknown <ilianjesus14@outlook.com>
2024-08-21 18:02:57 -0600

    Handle errors at signup

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
 
@@ -11,19 +13,23 @@ def home(request):
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+            'form': UserCreationForm
+        })
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                ##Cramos el usuario
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
-                ##Guardamos el usuarioF
-                user.save() 
+                # Cramos el usuario
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                # Guardamos el usuarioF
+                user.save()
                 return HttpResponse('Usuario creado satisfactoriamente')
             except:
-                return HttpResponse('El usuario ya existe')
-        return HttpResponse('Las contraseñas no coinciden')
-
-        
-    
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    "error" : "El usuario ya existe"
+                })
+        return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    "error" : "Las contraseñas no coinciden"
+                })
